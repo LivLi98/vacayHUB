977b8581672c208fa8f06f10683e6c5db9d267e0
Conrad Quagliaroli <conradbquag@gmail.com>
2023-04-04 10:59:31 -0400

    added dates

--- script.js
+++ script.js
@@ -2,14 +2,49 @@
 let url = 'http://api.weatherapi.com/v1';
 let apiKey ='?key=c021b43ef7ef412eac6134345233003&q=';
 
-let withinFourteen;
+
+let today = new Date();
+let todaysDay=today.getDate();
+let todaysMonth=today.getMonth();
+let todaysYear=today.getFullYear();
+
+
+let withinFourteen=true;
+
 
 let cityInputElement=document.getElementById('city-input');
-let startDataElement=document.getElementById('start');
+let startDateElement=document.getElementById('start');
 let endDateElement=document.getElementById('end');
-let submitButton=document.getElementById('submit-data')
+let submitButton=document.getElementById('submit-data');
+
+let vacationDataBox=document.querySelector('.api-data');
+
+
 
 submitButton.addEventListener('click',(e)=>{
-    let cityValue=cityInputElement.value.toLowerCase();
-    console.log(cityValue);
+    console.log(cityInputElement.value)
+    
+    let cityValue=cityInputElement.value;
+    let startDate=startDateElement.value;
+    let endDate=endDateElement.value;
+
+    let sDate=new Date(startDate);
+    let currentDate;
+    let eDate=new Date(endDate);
+
+    let difference=eDate.getTime()-sDate.getTime();
+    let daysBetween=Math.ceil(difference/(1000*3600*24));
+    console.log(daysBetween);
+
+
+    //getting data within 14 days
+    fetch(`${url}/forecast.json${apiKey}${cityValue}&dt=${startDate}`)
+        .then(response=>response.ok?response.json():null)
+        .then(data=>{
+            vacationDataBox.innerHTML=`
+            <div>${data.forecast.forecastday[0].date}</div>
+            <div>${data.forecast.forecastday[0].day.maxtemp_f}</div>`
+        })
+    
+    
 })
