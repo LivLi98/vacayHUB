1863719cc9dc8039f4e3dfc5e8ee8eddaf052e2f
Conrad Quagliaroli <conradbquag@gmail.com>
2023-04-07 09:48:57 -0400

    added comments to explain functionality better

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ let tripSug=document.createElement('div');
 let vacationDataBox=document.querySelector('.api-data');
 let imageBox=document.querySelector('.changing-images');
 
+//rapid api's way of using the api keys
 const options = {
 	method: 'GET',
 	headers: {
@@ -36,18 +37,8 @@ const options = {
 	}
 }
 
-
-let buildVacationDay=async(dataSet)=>{
-   return{
-        date: dataSet.forecast.forecastday[0].date.slice(5),
-        condition: dataSet.forecast.forecastday[0].day.condition.icon,
-        high: dataSet.forecast.forecastday[0].day.maxtemp_f,
-        low: dataSet.forecast.forecastday[0].day.mintemp_f,
-        toDo: []
-    }
-}
-
-
+//takes the information from a fully built up array of day objects and innerhtml's
+//them onto the page
 let buildHTML=(vacObj,num)=>{
     let count=num;
     vacationDataBox.innerHTML='';
@@ -56,6 +47,7 @@ let buildHTML=(vacObj,num)=>{
     tripWeather.setAttribute('class', 'weather-box')
     tripSug.setAttribute('class', 'event-box');
     vacObj.forEach(vac=>{
+        //limits the display of data to 'num' days on the home page
         if(count>0){
             tripWeather.innerHTML+=`
             <div class = "weather">
@@ -78,16 +70,46 @@ let buildHTML=(vacObj,num)=>{
 }
 
 
-let daysBetween=(start,end)=>Math.ceil((end.getTime()-start.getTime())/(1000*3600*24));
+//fetches a trip plan from an ai planner api. this is very slow
+let fetchEvents=async(numdays,cityname)=>{
+    return new Promise(resolve=>{
+        fetch(`https://ai-trip-planner.p.rapidapi.com/?days=${numdays}&destination=${cityname}`, options)
+        .then(response => response.json())
+        .then(response =>{
+            let dayArray=[];
+            response.plan.forEach(pl=>{
+                dayArray.push(pl.activities);         
+            })
+            resolve(dayArray);       
+        }).catch(err => console.error(err));
+    })
+}
 
 
+//takes the information from a fetch call and builds an object with information inside
+let buildVacationDay=async(dataSet)=>{
+    return{
+         date: dataSet.forecast.forecastday[0].date.slice(5),
+         condition: dataSet.forecast.forecastday[0].day.condition.icon,
+         high: dataSet.forecast.forecastday[0].day.maxtemp_f,
+         low: dataSet.forecast.forecastday[0].day.mintemp_f,
+         toDo: []
+     }
+ }
+
+
+//fetches weathether information from an api and returns the resulting object from a 
+//function call
 let fetchDays=async(cName,sD)=>{
     return new Promise(resolve=>{
 
         let fromToday=daysBetween(today,sD);
 
+        //converts the date given by the date input into a format usable by the weather api
         let startDateFormat=`${sD.getFullYear()}-${(sD.getMonth()+1)<10?`0${sD.getMonth()+1}`:sD.getMonth()+1}-${sD.getDate()<10?`0${sD.getDate()}`:sD.getDate()}`
         
+        //there are two different calls to the api that only work before and after 14 days from now, 
+        //and this decides between the two based on the current day
         if(fromToday<16){
             fetch(`${url}/forecast.json${apiKey}${cName}&dt=${startDateFormat}`)
             .then(response=>response.ok?response.json():null)
@@ -105,26 +127,20 @@ let fetchDays=async(cName,sD)=>{
 }
 
 
-let fetchEvents=async(numdays,cityname)=>{
-    return new Promise(resolve=>{
-        fetch(`https://ai-trip-planner.p.rapidapi.com/?days=${numdays}&destination=${cityname}`, options)
-        .then(response => response.json())
-        .then(response =>{
-            let dayArray=[];
-            response.plan.forEach(pl=>{
-                dayArray.push(pl.activities);         
-            })
-            resolve(dayArray);       
-        }).catch(err => console.error(err));
-    })
-}
+//takes the two given times, converts them to their pure numerical values to compare, 
+//then converts the difference back
+let daysBetween=(start,end)=>Math.ceil((end.getTime()-start.getTime())/(1000*3600*24));
 
 
+//takes a city name, start and end date, then fetches information for each of those days,
+//then appends that information to the screen and saves that info to local storage
 let buildDayInfo=async(cName,sD,eD)=>{
 
     numVacationDays=daysBetween(sD,eD);
 
     if(numVacationDays>0){
+        //iterates down through the available days and fetches new weather information for 
+        //each of those days, then pushes them into an array of objects
         for(let x=0;x<numVacationDays;x++){
             sD.setDate(sD.getDate()+1);
             let fetcher=await fetchDays(cName,sD);
@@ -133,6 +149,8 @@ let buildDayInfo=async(cName,sD,eD)=>{
         }
     }else{vacationDataBox.innerHTML='Invalid Dates'}
 
+    //this will do the same with plans, but the api can give information
+    //for the whole range instead of just one day
     let getEventsList= await fetchEvents(numVacationDays,cName);
 
     for(let x=0;x<numVacationDays;x++){
@@ -140,17 +158,19 @@ let buildDayInfo=async(cName,sD,eD)=>{
     }
 
     buildHTML(vacationDayObjects,7);
-
     console.log(vacationDayObjects, 'vacation day built');
 
+    //takes the array of objects that's been built and formats them for JSON,
+    //then adds that to local storage to be used by the calendar
     dayObjectsJSON= JSON.stringify(vacationDayObjects);
-
     console.log(dayObjectsJSON, 'json');
-
     localStorage.setItem('vacationDayObjectsJ',dayObjectsJSON);
 }
 
 
+
+//when the submit button is pressed, the city name, start and end date are fed
+//to the buildDayInfo function, which is the heart of the program
 submitButton.addEventListener('click',(e)=>{
 
     imageBox.style.display='none';
